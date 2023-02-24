53db27a0ebf322e31b00f55f05655f241326162f
pal-anish <anishpal25@gmail.com>
2023-02-24 21:28:48 +0530

    Added

--- src/Components/bootApi.js
+++ src/Components/bootApi.js
@@ -1,2 +1,2 @@
-const base_url="http://ec2-18-212-55-150.compute-1.amazonaws.com:8083/api/v1.0/tweets";
+const base_url="http://ec2-35-78-209-208.ap-northeast-1.compute.amazonaws.com:8083/api/v1.0/tweets";
 export default base_url;
